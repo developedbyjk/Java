d1cca360975fefaf589f3350e45330b40d164576
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-09 20:04:00 +0530

    control statement

---
+++ controlStatement.java
@@ -0,0 +1,11 @@
+public class controlStatement {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 0;
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("b is greater than a");
+        }
+    }
+}
