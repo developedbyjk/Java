c9ceac40ac03755e7ad979e95e5540e86d0145d1
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-09 20:54:02 +0530

    commit

---
+++ scope.java
@@ -0,0 +1,14 @@
+class myscope {
+    int a = 10;
+    static int b = 0;
+
+    void display() {
+        System.out.println("a is greater than b");
+    }
+}
+
+public class scope {
+    public static void main(String[] args) {
+
+    }
+}
