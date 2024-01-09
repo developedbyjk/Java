00b0816e3d82bce5fff0ac90fde84969c8db27a9
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-09 19:56:12 +0530

    testing errors

---
+++ SimpleApplete.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("Hello, World!", 20, 20);
+    }
+}
---
+++ errorcheck.java
@@ -0,0 +1,8 @@
+public class errorcheck {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int result = a / b;
+        System.out.println(result);
+    }
+}
