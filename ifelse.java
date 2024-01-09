c73a8f47c48a7fe29353c45947fd7d1391419da8
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-01-09 21:00:18 +0530

    switch statement

---
+++ ifelse.java
@@ -0,0 +1,24 @@
+public class ifelse {
+    public static void main(String args[]) {
+        for (int i = 0; i < 5; i++) {
+            switch (i) {
+                case 1:
+                    System.out.println("i is 1");
+                    break;
+                case 2:
+                    System.out.println("i is 2");
+                    break;
+                case 3:
+                    System.out.println("i is 3");
+                    break;
+                case 4:
+                    System.out.println("i is 4");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+}
